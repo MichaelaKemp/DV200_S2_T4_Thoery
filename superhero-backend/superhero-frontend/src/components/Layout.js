800ea0ecc1d2f57bc20c69a74b149d43dc3edd1a
MichaelaKemp <125548449+MichaelaKemp@users.noreply.github.com>
2024-10-19 12:57:08 +0200

    Adjusting to different screen sizes

--- superhero-backend/superhero-frontend/src/components/Layout.js
+++ superhero-backend/superhero-frontend/src/components/Layout.js
@@ -1,25 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Layout.css';
 import { Link } from 'react-router-dom';
-import logo from '../assets/supers_logo.png'; // Update this path based on your project structure
+import logo from '../assets/supers_logo.png';
 
 const Layout = ({ children }) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <div className="layout-container">
       <nav className="navbar">
         <div className="navbar-brand">
-          {/* Logo link */}
           <Link to="/">
             <img src={logo} alt="Logo" className="logo" />
           </Link>
           <h1 className="navbar-title">Supers</h1>
         </div>
-        <ul className="navbar-links">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/About">About</Link></li>
-          <li><Link to="/Characters">Characters</Link></li>
-          <li><Link to="/Comparison">Comparison</Link></li>
-          <li><Link to="/Feedback">Feedback</Link></li>
+        <button className="burger-menu" onClick={toggleMenu}>
+          ☰
+        </button>
+        <ul className={`navbar-links ${isMenuOpen ? 'navbar-active' : ''}`}>
+          <li><Link to="/" onClick={toggleMenu}>Home</Link></li>
+          <li><Link to="/About" onClick={toggleMenu}>About</Link></li>
+          <li><Link to="/Characters" onClick={toggleMenu}>Characters</Link></li>
+          <li><Link to="/Comparison" onClick={toggleMenu}>Comparison</Link></li>
+          <li><Link to="/Feedback" onClick={toggleMenu}>Feedback</Link></li>
         </ul>
       </nav>
       <div className="content">{children}</div>
