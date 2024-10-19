292e02dc2cb06c08698e87c79d35a3bc26bffc42
MichaelaKemp <125548449+MichaelaKemp@users.noreply.github.com>
2024-10-19 12:10:50 +0200

    Speeding up processes

--- superhero-backend/server.js
+++ superhero-backend/server.js
@@ -19,13 +19,13 @@ app.use(cors({
 }));
 
 // Enable preflight requests for all routes
-app.options('*', (req, res) => {
-  res.header('Access-Control-Allow-Origin', 'https://nameless-temple-24409.herokuapp.com');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Access-Control-Allow-Headers');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.sendStatus(200);
-});
+// app.options('*', (req, res) => {
+//   res.header('Access-Control-Allow-Origin', 'https://nameless-temple-24409.herokuapp.com');
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Access-Control-Allow-Headers');
+//   res.header('Access-Control-Allow-Credentials', 'true');
+//   res.sendStatus(200);
+// });
 
 // Parse incoming JSON requests
 app.use(express.json()); 
