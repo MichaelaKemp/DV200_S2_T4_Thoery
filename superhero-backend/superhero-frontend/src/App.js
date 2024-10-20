415d74609b17e69631d6cd244e893667f57c0fb5
MichaelaKemp <125548449+MichaelaKemp@users.noreply.github.com>
2024-10-20 17:41:10 +0200

    Fixing Google analytics

--- superhero-backend/superhero-frontend/src/App.js
+++ superhero-backend/superhero-frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import Characters from './components/Characters';
 import Feedback from './components/Feedback';
@@ -7,29 +7,18 @@ import About from './components/About';
 import Details from './components/Details';
 import Comparison from './components/Comparison';
 import axios from 'axios';
-import { initGA, logPageView } from './components/Analytics'; // Import Google Analytics functions
 
 function App() {
   const [superheroes, setSuperheroes] = useState([]);
-  const location = useLocation();
 
   useEffect(() => {
-    // Initialize Google Analytics once when the app mounts
-    initGA();
-
-    // Fetch data from the backend
+    // Fetch data from the backend once
     const fetchSuperheroes = async () => {
       const result = await axios.get('https://supers-hub-b2090f2bdf4f.herokuapp.com/api/superheroes');
       setSuperheroes(result.data.results);  // Assuming API returns 'results' array
     };
-    // Uncomment this line if you want to fetch superheroes data
-    // fetchSuperheroes();
+    //fetchSuperheroes();
   }, []);
-
-  useEffect(() => {
-    // Log page view when the route changes
-    logPageView(location.pathname);
-  }, [location]);
 
   return (
     <Router>
@@ -39,7 +28,7 @@ function App() {
         <Route path="/feedback" element={<Feedback />} />
         <Route path="/about" element={<About />} />
         <Route path="/comparison" element={<Comparison />} />
-        <Route path="/details/:id" element={<Details />} />
+        <Route path="/details/:id" element={<Details />} /> {/* Add this line */}
       </Routes>
     </Router>
   );
