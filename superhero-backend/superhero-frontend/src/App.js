2943b02d26da773357d5fce2b7d0765239743d8e
MichaelaKemp <125548449+MichaelaKemp@users.noreply.github.com>
2024-10-20 17:28:30 +0200

    Fixing Google analytics

--- superhero-backend/superhero-frontend/src/App.js
+++ superhero-backend/superhero-frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import Home from './components/Home';
 import Characters from './components/Characters';
 import Feedback from './components/Feedback';
@@ -7,18 +7,29 @@ import About from './components/About';
 import Details from './components/Details';
 import Comparison from './components/Comparison';
 import axios from 'axios';
+import { initGA, logPageView } from './components/Analytics'; // Import Google Analytics functions
 
 function App() {
   const [superheroes, setSuperheroes] = useState([]);
+  const location = useLocation();
 
   useEffect(() => {
-    // Fetch data from the backend once
+    // Initialize Google Analytics once when the app mounts
+    initGA();
+
+    // Fetch data from the backend
     const fetchSuperheroes = async () => {
       const result = await axios.get('https://supers-hub-b2090f2bdf4f.herokuapp.com/api/superheroes');
       setSuperheroes(result.data.results);  // Assuming API returns 'results' array
     };
-    //fetchSuperheroes();
+    // Uncomment this line if you want to fetch superheroes data
+    // fetchSuperheroes();
   }, []);
+
+  useEffect(() => {
+    // Log page view when the route changes
+    logPageView(location.pathname);
+  }, [location]);
 
   return (
     <Router>
@@ -28,7 +39,7 @@ function App() {
         <Route path="/feedback" element={<Feedback />} />
         <Route path="/about" element={<About />} />
         <Route path="/comparison" element={<Comparison />} />
-        <Route path="/details/:id" element={<Details />} /> {/* Add this line */}
+        <Route path="/details/:id" element={<Details />} />
       </Routes>
     </Router>
   );
